4480d771933c66c2a94f6c443c8d79cb4588e960
vivan-j <vivan.j924@gmail.com>
2025-07-16 12:17:31 +0530

    Create App.jsx

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function App() {
+  return (
+    <div>
+      <h1>Hello Vite + React!</h1>
+      <p>Edit <code>src/App.jsx</code> and save to test HMR updates.</p>
+    </div>
+  )
+}
+
+export default App
